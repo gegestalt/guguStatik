1f27fc4b3addb2ce4de6050d79c33ac3d26aadc0
gunal4 <gunal@meikosoft.com>
2024-04-28 12:09:44 +0300

    added the "gugustatik" banner

--- basic_static_tool/init1.py
+++ basic_static_tool/init1.py
@@ -8,6 +8,29 @@ import py7zr
 
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+
+def print_greeting():
+    print('''
+                                                         .x+=:.        s                     s       .          ..      
+                                                        z`    ^%      :8                    :8      @88>  < .z@8"`      
+               x.    .                    x.    .          .   <k    .88                   .88      %8P    !@88E        
+     uL      .@88k  z88u        uL      .@88k  z88u      .@8Ned8"   :888ooo       u       :888ooo    .     '888E   u    
+ .ue888Nc.. ~"8888 ^8888    .ue888Nc.. ~"8888 ^8888    .@^%8888"  -*8888888    us888u.  -*8888888  .@88u    888E u@8NL  
+d88E`"888E`   8888  888R   d88E`"888E`   8888  888R   x88:  `)8b.   8888    .@88 "8888"   8888    ''888E`   888E`"88*"  
+888E  888E    8888  888R   888E  888E    8888  888R   8888N=*8888   8888    9888  9888    8888      888E    888E .dN.   
+888E  888E    8888  888R   888E  888E    8888  888R    %8"    R88   8888    9888  9888    8888      888E    888E~8888   
+888E  888E    8888 ,888B . 888E  888E    8888 ,888B .   @8Wou 9%   .8888Lu= 9888  9888   .8888Lu=   888E    888E '888&  
+888& .888E   "8888Y 8888"  888& .888E   "8888Y 8888"  .888888P`    ^%888*   9888  9888   ^%888*     888&    888E  9888. 
+*888" 888&    `Y"   'YP    *888" 888&    `Y"   'YP    `   ^"F        'Y"    "888*""888"    'Y"      R888" '"888*" 4888" 
+ `"   "888E                 `"   "888E                                       ^Y"   ^Y'               ""      ""    ""   
+.dWi   `88E                .dWi   `88E                                                                                  
+4888~  J8%                 4888~  J8%                                                                                   
+ ^"===*"`                   ^"===*"`                                                                                   
+    ''')
+
+print_greeting()
+
+
 def identify_file_type(file_path):
     mime = magic.Magic(mime=True)
     return mime.from_file(file_path)
