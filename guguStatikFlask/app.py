09917b3f0d9de7b94c8ce4d6d4aa88d20c10f731
gunal4 <gunal@meikosoft.com>
2024-05-11 05:52:09 +0300

    VT perfecting mate

--- guguStatikFlask/app.py
+++ guguStatikFlask/app.py
@@ -82,7 +82,9 @@ def submit_file():
 def display_results(analysis_id):
     response = get_analysis_results(analysis_id)
     results = response["data"]["attributes"]["results"]
-    return render_template("vt_report.html", results=results)
+    attributes = response["data"]["attributes"]
+    meta = response.get("meta", {}) 
+    return render_template("vt_report.html", results=results,meta=meta,attributes=attributes)
 
 def extract_info_from_pdf(file_path):
     with pdfplumber.open(file_path) as pdf:
